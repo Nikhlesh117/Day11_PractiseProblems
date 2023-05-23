2cc1ef9f9d1d137a7239db8187c86ddfbbdf5abf
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-23 18:58:57 +0530

    Added: CountOfRemainingChapters

---
+++ Collection/Collection/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    public class Book
+    {
+        private int[][] chapters;
+        private int totalChapters;
+
+        public Book(int[][] chapters)
+        {
+            this.chapters = chapters;
+            this.totalChapters = chapters.Length;
+        }
+
+        public int CountRemainingChapters(int currentPage)
+        {
+            int remainingChapters = 0;
+
+            for (int i = 0; i < totalChapters; i++)
+            {
+                int startPage = chapters[i][0];
+                int endPage = chapters[i][1];
+
+                if (currentPage >= startPage && currentPage <= endPage)
+                {
+                    remainingChapters = totalChapters - i;
+                    break;
+                }
+            }
+
+            return remainingChapters;
+        }
+    }
+}
---
+++ Collection/Collection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[][] chapters = new int[][]
+        {
+            new int[] { 1, 4 },
+            new int[] { 5, 7 },
+            new int[] { 9, 16 },
+            new int[] { 17, 26 }
+        };  // Start and end page of each chapter
+
+            int currentPage = 7;  // Current reading page
+
+            Book book = new Book(chapters);
+            int remainingChapters = book.CountRemainingChapters(currentPage);
+            Console.WriteLine("Remaining Chapter : " + remainingChapters);
+
+            Console.ReadLine();
+        }
+    }
+}
